ac3f45b1fe25a641e2f40232e461f8c2572f2606
shashankyadav03 <shashankyadav97@yahoo.in>
2020-08-26 19:50:51 +0530

    More practice

--- Basics/main.go
+++ Basics/main.go
@@ -1,31 +1,84 @@
 package main
 
-import "fmt"
+//var car int
 
-var car int
+// type person struct {
+// 	fname string
+// 	lname string
+// 	mob   contact
+// }
+// type contact struct {
+// 	email string
+// 	zip   int
+// }
 
 func main() {
+
+	// colors := make(map[string]bool)
+	// colors["yellow"] = true
+	// colors["red"] = true
+
+	// for color, ans := range colors {
+	// 	fmt.Println(color, ans)
+	// }
+
+	// fmt.Println(colors)
+	// colors := map[string]bool{
+	// 	"red":  true,
+	// 	"blue": true,
+	// }
+
+	//cvar colors map[string]string
+
+	// fmt.Println(colors)
+	// alco := contact{email: "sky@g.com", zip: 20022}
+	// alex := person{fname: "Alex", lname: "Tom", mob: alco}
+
+	// alex.updateName("AL")
+	// alex.print()
+
+	// alexP := &alex
+	// alexP.updateName("AL")
+	// alex.print()
+	//fmt.Printf("%+v", alex)
+
+	// alex := person{"Alex", "Anderson"}
+	// fmt.Println(alex)
+	// alex = person{fname: "Alex", lname: "Anderson"}
+	// fmt.Println(alex.fname)
+	// var al person
+	// fmt.Println(al)
+	// fmt.Printf("%+v", al)
+
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Hearts"
-	fmt.Println(card)
-	card = newCard()
-	fmt.Println(card)
+	// card := "Ace of Spades"
+	// card = "Hearts"
+	// fmt.Println(card)
+	// card = newCard()
+	// fmt.Println(card)
 
-	car = 9
-	fmt.Println(car)
+	// car = 9
+	// fmt.Println(car)
 
-	cards := []string{newCard(), "Ace"}
-	fmt.Println(cards)
-	cards = append(cards, "Six")
-	fmt.Println(cards)
+	// cards := []string{newCard(), "Ace"}
+	// fmt.Println(cards)
+	// cards = append(cards, "Six")
+	// fmt.Println(cards)
 
-	for i := range cards {
-		fmt.Println(cards[i])
-	}
+	// for i := range cards {
+	// 	fmt.Println(cards[i])
+	// }
 
 }
 
-func newCard() string {
-	return "five"
-}
+// func (pToP *person) updateName(newf string) {
+// 	(*pToP).fname = newf
+// }
+
+// func (p person) print() {
+// 	fmt.Printf("%+v\n", p)
+// }
+
+// func newCard() string {
+// 	return "five"
+// }
